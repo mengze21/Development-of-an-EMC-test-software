23ed9d9343a26d55f7c69c08dfd3f1a51621296f
MiPa-GT <49078382+MiPa-GT@users.noreply.github.com>
2022-02-11 15:22:15 +0100

    current version calibration measurement

--- gui/calibrationRun.py
+++ gui/calibrationRun.py
@@ -1,15 +1,16 @@
 # Import the Driver for meas Devices
 import sys
 import csv
-import time
-from typing import List
 
+from typing import List
 import pyvisa
 
 from WrapperEMRFeldsonde import WrapperEMRFeldsonde
 from WrapperSwitchHP import WrapperSwitchHP
 from WrapperPowerMeter import WrapperPowerMeter
 from WrapperSignalGenerator import WrapperSignalGenerator
+
+from tqdm import tqdm
 #plot stuiff
 import matplotlib
 matplotlib.use("TkAgg")
@@ -20,21 +21,21 @@ import time
 sys.path.append('gui')
 # start Values
 startFrequ = 80
-endFrequ = 210
+endFrequ = 1000
 frequStep = 1.01
 polarisation = 'vertical'
-E_T = 10  # Prüffeldstärke
+E_T = 3 # Prüffeldstärke
 E_L = E_T * 1.8  # Pegeleinstellungsfeldstärke
-E_L_Tol = 0.5
+E_L_Tol = 0.6
 max_E_L = E_L + E_L_Tol
 control_E_L = E_L + E_L_Tol/2
 totalPoints = 5
-startAPM = -20
+startAPM = -27
 stepAPM = 0
 # maxAPMPoss = -10
 powMeterTol = 0.5
 activSwitch = 0
-maxChangeVal = 7
+maxChangeVal = 10
 prevStepAPM = startAPM
 
 # Driver selection
@@ -52,6 +53,8 @@ powFwdVal = []
 sondeListVal = []
 gridValFrequ = []
 gridValFwd = []
+powRevVal = []
+powAPMSet  = []
 
 def abortTest():
     instSigGen.switchRFOff()
@@ -92,74 +95,87 @@ def PI(Kp, Ki, MV_bar, beta):
         # PI calculations
         P = Kp * (beta * SP - PV)
         I = I + Ki * (SP - PV) * (t - t_prev)
-        print('P: %f' % P)
-        print('I: %f' % I)
+        # print('P: %f' % P)
+        # print('I: %f' % I)
         MV = MV_bar + P + I
 
-        print('New MV: %f' % MV)
+        # print('New MV: %f' % MV)
         # update stored data for next iteration
         t_prev = t
 
 
 
-
-for currPoint in range(1, totalPoints + 1, 1):
+ii = 1
+for currPoint in range(2, totalPoints + 1, 1):
 
     frequVal.clear()
     powFwdVal.clear()
+    powRevVal.clear()
     sondeListVal.clear()
     setFrequ = startFrequ
+    setAPM = startAPM
     flgGridPoint = input('Feldsonde aufgestellt?: \n')
     if flgGridPoint == 'yes':
         currAmpSwitch = instSwitch.validFrequ(setFrequ) # check if frequency is valid
         if currAmpSwitch == 1:
             instSwitch.switchAmp1()
             activSwitch = 1
-            print('activeSwitch: %i \n' % activSwitch)
+            print('activeSwitch: %i ' % activSwitch)
         elif currAmpSwitch == 2:
             instSwitch.switchAmp2()
             activSwitch = 2
-            print('activeSwitch: %i \n' % activSwitch)
+            print('activeSwitch: %i' % activSwitch)
         elif currAmpSwitch == 3:
             instSwitch.switchAmp3()
             activSwitch = 3
-            print('activeSwitch: %i \n' % activSwitch)
+            print('activeSwitch: %i ' % activSwitch)
         elif currAmpSwitch == 4:
-            print('activeSwitch Abort: %i \n' % activSwitch)
+            print('activeSwitch Abort: %i ' % activSwitch)
             break
 
+        frequList = []
         while setFrequ <= endFrequ:
+            frequList.append(setFrequ)
+            setFrequ = setFrequ * 1.01
 
+        for i in tqdm(frequList, desc ="Calibration Frequ Step:"):
+            setFrequ = i
             plt.ion()  ## Note this correction
             fig = plt.figure()
-            plt.axis([0, 60, 13, 21])
+            plt.title("constant field strength control")
+            plt.xlabel("Step")
+            plt.ylabel("field strength")
+            plt.axis([0, 60, 2, 9])
+            plt.axhline(y=control_E_L, color='b', linestyle='-')
+            plt.axhline(y=max_E_L, color='r', linestyle='-')
+            plt.axhline(y=E_L, color='g', linestyle='-')
 
             MV = setAPM
-            print('SetFrequ: %f \n' % setFrequ)
+            # print('SetFrequ: %f' % setFrequ)
             instSigGen.switchRFOff() # set RF ON!!!!!!!!!
             controller = PI(0.2, 0.033, MV, 1)
             controller.send(None)
             # time.sleep(20)
             # check if the switch needs to be switched
             currAmpSwitch = instSwitch.validFrequ(setFrequ)  # check if frequency is valid
+            # print('Needed Amplfiier: %i' % currAmpSwitch)
             if currAmpSwitch != activSwitch:
                 if currAmpSwitch == 1:
                     instSwitch.switchAmp1()
                     activSwitch = 1
-                    print('activeSwitch: %i \n' % activSwitch)
+                    print('activeSwitch: %i' % activSwitch)
                 elif currAmpSwitch == 2:
                     instSwitch.switchAmp2()
                     activSwitch = 2
-                    print('activeSwitch: %i \n' % activSwitch)
+                    print('activeSwitch: %i' % activSwitch)
                 elif currAmpSwitch == 3:
                     instSwitch.switchAmp3()
                     activSwitch = 3
-                    print('activeSwitch: %i \n ' % activSwitch)
+                    print('activeSwitch: %i' % activSwitch)
                 elif currAmpSwitch == 4:
-                    print('activeSwitch Abort: %i \n' % activSwitch)
+                    print('activeSwitch Abort: %i' % activSwitch)
                     abortTest()
                     break
-
             instSigGen.setFrequMHZ(setFrequ)  # Set Beginning frequency
             # if setAPM > maxAPMPoss:
             #     abortTest()
@@ -178,17 +194,30 @@ for currPoint in range(1, totalPoints + 1, 1):
 
 
             # Now do the loop to find the correct E_L Value
-            print('currSondeVal: %f' % currSondeVal)
-            print('CurrFwdVal: %f' % currFwdVal)
-            print('currRevVal: %f' % currRevVal)
+            # print('currSondeVal: %f' % currSondeVal)
+            # print('CurrFwdVal: %f' % currFwdVal)
+            # print('currRevVal: %f' % currRevVal)
             k = 1
             startTime = time.time()
+            iRow = 1
+            tmpSondeVal = [False]*3
             while 1:
+                # quadrieren der toleranzen -> kein sqrt nötig
                 if (E_L < currSondeVal < max_E_L):
-                    # do at least 12 steps to guarentee uniform field
-                    if k>12:
-                        break
-                if (k % 50) == 0:
+                    # find at least 3 Sondeval in Row to validate
+                    tmpSondeVal[iRow] = True
+                else:
+                    tmpSondeVal[iRow] = False
+
+                # print(tmpSondeVal)
+                iRow = iRow + 1
+                if iRow > 2:
+                    iRow = 0
+
+                if all(tmpSondeVal):
+                    break
+
+                if (k % 60) == 0:
                     continueTest = input('Continue Test?\n')
                     if continueTest == 'No':
                         abortTest()
@@ -209,6 +238,7 @@ for currPoint in range(1, totalPoints + 1, 1):
                 # piyes = input('set MV TO APM?')
                 # if piyes == 'yes':
                 #     print('set APM to %f' % MV)
+                # print('set APM to %f' % MV)
                 setAPM = MV
 
                 if abs(abs(prevStepAPM) - abs(setAPM)) > maxChangeVal:
@@ -221,22 +251,22 @@ for currPoint in range(1, totalPoints + 1, 1):
                 timeSonde = time.time()
                 instSonde.readval()
                 currSondeVal = instSonde.effEVal
-                print('time sonde:%f' % (time.time()-timeSonde))
-                print('Updated currSondeVal: %f ' % currSondeVal)
+                # print('time sonde:%f' % (time.time()-timeSonde))
+                # print('Updated currSondeVal: %f ' % currSondeVal)
                 t = time.time()-startTime
                 MV = controller.send([t, currSondeVal, control_E_L])
-                if (k > 8) and ((k % 4) == 0):
+                if (k > 5) and ((k % 3) == 0):
                     timePowMetA = time.time()
                     instPowerMeter.switchChannelA()
                     instPowerMeter.getMeasVal()
                     currFwdVal = instPowerMeter.currVal
-                    print('time PowerMeterA:%f' % (time.time()-timePowMetA))
+                    #print('time PowerMeterA:%f' % (time.time()-timePowMetA))
                     # print('Updated CurrFwdVal: %f \n' % currFwdVal)
                     timePowMetB = time.time()
                     instPowerMeter.switchChannelB()
                     instPowerMeter.getMeasVal()
                     currRevVal = instPowerMeter.currVal
-                    print('time PowerMeterB:%f' % (time.time()-timePowMetB))
+                    #print('time PowerMeterB:%f' % (time.time()-timePowMetB))
                 # print('Updated currRevVal: %f \n' % currRevVal)
                 k = k + 1
                 plt.scatter(k, currSondeVal)
@@ -246,26 +276,33 @@ for currPoint in range(1, totalPoints + 1, 1):
 
 
             # save the values in lists
-            plt.close()
+
             instPowerMeter.switchChannelA()
             instPowerMeter.getMeasVal()
             currFwdVal = instPowerMeter.currVal
+            instPowerMeter.switchChannelB()
+            instPowerMeter.getMeasVal()
+            currRevVal = instPowerMeter.currVal
             instSonde.readval()
             currSondeVal = instSonde.effEVal
             controller.close()
             frequVal.append(setFrequ)
             powFwdVal.append(currFwdVal)
+            powRevVal.append(currRevVal)
             sondeListVal.append(currSondeVal)
-            print('Saved currFwdVal: %f \n' % currFwdVal)
-            setFrequ = setFrequ * frequStep
-            print('Next Frequ setFrequ: %f \n' % setFrequ)
-            res = [l for l in zip(frequVal, powFwdVal, sondeListVal)]
-            with open('output.csv', 'w', newline='') as csvfile:
+            powAPMSet.append(setAPM)
+            plt.savefig('graphs/Field_strength_%s_%i_%i' % (polarisation, currPoint, ii))
+            plt.close()
+            # print('Saved currFwdVal: %f' % currFwdVal)
+            res = [l for l in zip(frequVal, powFwdVal, powRevVal, powAPMSet, sondeListVal)]
+            path = 'output_%s_%i.csv' % (polarisation, currPoint)
+            with open(path, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 for l in res:
                     writer.writerow(l)
-
+            ii = ii + 1
     else:
         break
+
     gridValFrequ.append(frequVal)
     gridValFwd.append(powFwdVal)
