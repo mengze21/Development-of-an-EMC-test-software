d5c1e0caeaf0d6844cce7e679eb10236f6eafd93
MiPa-GT <49078382+MiPa-GT@users.noreply.github.com>
2022-02-24 14:43:30 +0100

    Add files via upload

--- gui/measurementRun.py
+++ gui/measurementRun.py
@@ -11,11 +11,14 @@ from WrapperEMRFeldsonde import WrapperEMRFeldsonde
 from WrapperSwitchHP import WrapperSwitchHP
 from WrapperPowerMeter import WrapperPowerMeter
 from WrapperSignalGenerator import WrapperSignalGenerator
+from WrapperEMRFeldsonde import WrapperEMRFeldsonde
+
 #plot stuiff
 
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2
 
 import time
 
@@ -31,10 +34,12 @@ rm = pyvisa.ResourceManager()
 instPowerMeter = WrapperPowerMeter('GPIB0::11::INSTR', rm)
 instSwitch = WrapperSwitchHP('GPIB0::9::INSTR', rm)
 instSigGen = WrapperSignalGenerator('GPIB0::21::INSTR', rm)
+instWrapperEMR = WrapperEMRFeldsonde("ASRL3::INSTR", rm) # Adresse als Variable von GUI übernehmen
 
 # list creation
 powFwdVal = []
 powRevVal = []
+
 
 def abortTest():
     instSigGen.switchRFOff()
@@ -75,11 +80,11 @@ def PI(Kp, Ki, MV_bar, beta):
         # PI calculations
         P = Kp * (beta * SP - PV)
         I = I + Ki * (SP - PV) * (t - t_prev)
-        print('P: %f' % P)
-        print('I: %f' % I)
+        # print('P: %f' % P)
+        # print('I: %f' % I)
         MV = MV_bar + P + I
 
-        print('New MV: %f' % MV)
+        # print('New MV: %f' % MV)
         # update stored data for next iteration
         t_prev = t
 
@@ -88,7 +93,7 @@ def PI(Kp, Ki, MV_bar, beta):
 
 # Read the csv file as rows in the list.
 # Format is the following frequency, Forward Power , Reverse Power, Power for the SignalGenerator, Electric Field Strength.
-with open('output.csv', 'r') as read_obj:
+with open('calResult.csv', 'r') as read_obj:
     csv_reader = reader(read_obj)
     listCalFile = list(csv_reader)
     print(listCalFile)
@@ -107,10 +112,19 @@ if testType == '1':
     # 2 = Reverse Power
     # 3 = Power Signal Generator
     # 4 = Electric Field Strength
+    # 5 = Cal Success Status
 
+    plt.ion()  ## Note this correction
+    fig = plt.figure()
+    plt.title("Field Strength Control")
+    plt.xlabel("Step")
+    plt.ylabel("Field Strength")
+    plt.axis([0, 200, -3, 10])
+    plt.axhline(y=(3), color='r', linestyle='-')
+    k = 1
     for x in listCalFile:
-        setFrequ = x[0]
-        setAPM = x[3]
+        setFrequ = float(x[0])
+        setAPM = float(x[3])
         currAmpSwitch = instSwitch.validFrequ(setFrequ)  # check if frequency is valid
         if currAmpSwitch != activSwitch:
             if currAmpSwitch == 1:
@@ -132,14 +146,35 @@ if testType == '1':
 
         instSigGen.setFrequMHZ(setFrequ)  # Set Frequency
         instSigGen.setAmpDBM(setAPM) # set amplitude value from Cal File
-        instSigGen.switchRFOn()  # set RF ON!!!!!!!!
+        instSigGen.switchRFOn()  # set RF ON!!!!!!!
+        instWrapperEMR.readval()
+        plt.scatter(k, instWrapperEMR.effEVal)
+        plt.show()
+        plt.pause(0.001)
+        instSigGen.switchAPModulationON()
         time.sleep(maxStayTime)
+        plt.scatter(k, instWrapperEMR.effEVal, marker=',')
+        k = k + 1
+        plt.show()
+        plt.pause(0.001)
+        instSigGen.switchAPModulationOFF()
+
+
 
 
 # Forward Power Calibration
 elif testType == '2':
-    fwdTol = 0.2
+    fwdTol = 0.1
     print('2 selected')
+    plt2.ion()
+    fig2 = plt2.figure()
+    plt2.title("Field Strength Control")
+    plt2.xlabel("Step")
+    plt2.ylabel("Field Strength")
+    plt2.axis([0, 200, -3, 10])
+    plt2.axhline(y=(3), color='r', linestyle='-')
+
+    j = 1
     for x in listCalFile:
         setFrequ = float(x[0])
         setFwdPow = float(x[1])
@@ -152,8 +187,10 @@ elif testType == '2':
         plt.title("Forward Power Control")
         plt.xlabel("Step")
         plt.ylabel("Forward Power")
-        plt.axis([0, 100, -23, -10])
+        plt.axis([0, 200, -35, -10])
         plt.axhline(y=(setFwdPow + fwdTol), color='r', linestyle='-')
+
+
         controller = PI(0.3, 0.22, MV, 1)
         controller.send(None)
         currAmpSwitch = instSwitch.validFrequ(setFrequ)  # check if frequency is valid
@@ -182,7 +219,7 @@ elif testType == '2':
         instPowerMeter.getMeasVal()
         currFwdVal = instPowerMeter.currVal
         k = 1
-        print('Goal Fwd Value: %f' % setFwdPow)
+        # print('Goal Fwd Value: %f' % setFwdPow)
         iRow = 1
         tmpFwdVal = [False] * 4
         while 1:
@@ -191,7 +228,7 @@ elif testType == '2':
             else:
                 tmpFwdVal[iRow] = False
 
-            print(tmpFwdVal)
+            # print(tmpFwdVal)
             iRow = iRow + 1
             if iRow > 3:
                 iRow = 0
@@ -202,24 +239,44 @@ elif testType == '2':
             instSigGen.setAmpDBM(MV)
             instPowerMeter.getMeasVal()
             currFwdVal = instPowerMeter.currVal
-            print('Current Fwd Value: %f' % currFwdVal)
+            # print('Current Fwd Value: %f' % currFwdVal)
             t = time.time() - startTime
-            MV = controller.send([t, currFwdVal, setFwdPow + fwdTol])
-            print('Control MV Value: %f' % MV)
+            MV = controller.send([t, currFwdVal, setFwdPow + 0.05])
+            # print('Control MV Value: %f' % MV)
             k = k + 1
             plt.scatter(k, currFwdVal)
             plt.show()
             plt.pause(0.001)
-
+        plt.close()
+        instWrapperEMR.readval()
+        print(instWrapperEMR.effEVal)
+        plt2.scatter(j, instWrapperEMR.effEVal)
+        plt2.show()
+        plt2.pause(0.001)
+        instSigGen.switchAPModulationON()
+        time.sleep(maxStayTime)
+        print('T',instWrapperEMR.effEVal)
+        plt2.scatter(j, instWrapperEMR.effEVal, marker=',')
+        j = j + 1
+        plt2.show()
+        plt2.pause(0.001)
+        instSigGen.switchAPModulationOFF()
         controller.close()
         powFwdVal.append(currFwdVal)
-        plt.close()
 
 
 
 elif testType == '3':
     print('3 selected')
     netPowTol = 0.3
+    plt2.ion()
+    fig2 = plt2.figure()
+    plt2.title("Field Strength Control")
+    plt2.xlabel("Step")
+    plt2.ylabel("Field Strength")
+    plt2.axis([0, 200, -3, 10])
+    plt2.axhline(y=(3), color='r', linestyle='-')
+
     for x in listCalFile:
         setFrequ = float(x[0])
         FwdPow = float(x[1])
@@ -235,7 +292,7 @@ elif testType == '3':
         plt.title("Net Power Control")
         plt.xlabel("Step")
         plt.ylabel("Net Power")
-        plt.axis([0, 100, -5, 12])
+        plt.axis([0, 200, -15, 15])
         plt.axhline(y=(setNetPower + netPowTol), color='r', linestyle='-')
         controller = PI(0.3, 0.22, MV, 1)
         controller.send(None)
@@ -271,7 +328,7 @@ elif testType == '3':
         currRevVal = instPowerMeter.currVal
         currNetPower = currFwdVal-currRevVal
         k = 1
-        print('Goal Fwd Value: %f' % setNetPower)
+        # print('Goal Fwd Value: %f' % setNetPower)
         iRow = 1
         tmpNetVal = [False] * 4
         while 1:
@@ -299,18 +356,33 @@ elif testType == '3':
 
             currNetPower = currFwdVal - currRevVal
 
-            print('Current Net Value: %f' % currNetPower)
+            # print('Current Net Value: %f' % currNetPower)
             t = time.time() - startTime
             MV = controller.send([t, currNetPower, setNetPower + netPowTol])
-            print('Control MV Value: %f' % MV)
+            # print('Control MV Value: %f' % MV)
             k = k + 1
             plt.scatter(k, currNetPower)
             plt.show()
             plt.pause(0.001)
+            instWrapperEMR.readval()
+            print(instWrapperEMR.effEVal)
 
+        plt.close()
+        instWrapperEMR.readval()
+        print(instWrapperEMR.effEVal)
+        plt2.scatter(j, instWrapperEMR.effEVal)
+        plt2.show()
+        plt2.pause(0.001)
+        instSigGen.switchAPModulationON()
+        time.sleep(maxStayTime)
+        print('T', instWrapperEMR.effEVal)
+        plt2.scatter(j, instWrapperEMR.effEVal, marker=',')
+        j = j + 1
+        plt2.show()
+        plt2.pause(0.001)
+        instSigGen.switchAPModulationOFF()
         controller.close()
         powFwdVal.append(currNetPower)
-        plt.close()
 
 # Abbruch calibration
 elif testType == '4':
